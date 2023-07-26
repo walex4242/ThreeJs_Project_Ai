dae18c8d812d9bed5b9a01f37394de541a703cb7
Olawale Olafisoye <walex.world20@gmail.com>
2023-07-25 21:22:58 -0300

    fix build

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -57,15 +57,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("https://mybackend-threejs.onrender.com", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+      const response = await fetch(
+        "https://mybackend-threejs.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
         },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      );
 
       const data = await response.json();
 
